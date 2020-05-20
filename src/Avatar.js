41584a282b7f442b5737784db0750838e006ae4c
Pawan Lamba <pawan88.lamba@gmail.com>
2020-05-19 23:12:39 -0400

    Creating Functional Component

---
+++ src/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Functional Component
+const Avatar = () =>{
+    return (<div>
+                <img src="" alt="avatar"/>
+                <h1> Pawan Lamba </h1>
+                <p> Web Developer</p>
+            </div>
+           )
+}
+
+export default Avatar;
