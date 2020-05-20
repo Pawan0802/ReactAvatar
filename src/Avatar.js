e4e88a43acb86f18891885557e84cafb1eba6336
Pawan Lamba <pawan88.lamba@gmail.com>
2020-05-20 00:05:14 -0400

    Loop the avatar list

--- src/Avatar.js
+++ src/Avatar.js
@@ -30,15 +30,15 @@ const Avatar = (props) =>{
         }
     ]
 
-    
+    //use map to loop
+    const avatarcard = avatarlistarray.map((avatarcard, i) => {
+        return <AvatarList id={avatarlistarray[i].id} name={avatarlistarray[i].name} work={avatarlistarray[i].work}/>
+    })
 
     return (
-            <div>
+            <div className="mainpage">
                 <h1>Welcome to Avatar World</h1>
-                <AvatarList id={avatarlistarray[0].id} name={avatarlistarray[0].name} work={avatarlistarray[0].work}/>
-                <AvatarList id={avatarlistarray[1].id} name={avatarlistarray[1].name} work={avatarlistarray[1].work}/>
-                <AvatarList id={avatarlistarray[2].id} name={avatarlistarray[2].name} work={avatarlistarray[2].work}/>
-                <AvatarList id={avatarlistarray[3].id} name={avatarlistarray[3].name} work={avatarlistarray[3].work}/>
+                {avatarcard}
                 <button>Subscribe</button>
             </div>
            )
