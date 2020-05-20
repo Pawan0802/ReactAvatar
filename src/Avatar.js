988d43b86e30c6848e4a9e58eedb616ea8ba9e4d
Pawan Lamba <pawan88.lamba@gmail.com>
2020-05-20 00:19:47 -0400

    Converting Functional Component to Class Component

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,47 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Avatar.css';
 import 'tachyons';
 import AvatarList from './AvatarList';
 
-//Functional Component
-const Avatar = (props) =>{
+//Class Component
+class Avatar extends Component{
+    render(){
+        //creating an array for avatar list
+        const avatarlistarray = [
+            {
+                id:1,
+                name:"Pawan",
+                work:"Full Stack Developer"
+            },
+            {
+                id:2,
+                name:"Rahul",
+                work:"FrontEnd Developer"
+            },
+            {
+                id:3,
+                name:"Punit",
+                work:"Database"
+            },
+            {
+                id:4,
+                name:"Jeetu",
+                work:"BackEnd Developer"
+            }
+        ]
 
-    //creating an array for avatar list
-    const avatarlistarray = [
-        {
-            id:1,
-            name:"Pawan",
-            work:"Full Stack Developer"
-        },
-        {
-            id:2,
-            name:"Rahul",
-            work:"FrontEnd Developer"
-        },
-        {
-            id:3,
-            name:"Punit",
-            work:"Database"
-        },
-        {
-            id:4,
-            name:"Jeetu",
-            work:"BackEnd Developer"
-        }
-    ]
+        //use map to loop
+        const avatarcard = avatarlistarray.map((avatarcard, i) => {
+            return <AvatarList id={avatarlistarray[i].id} name={avatarlistarray[i].name} work={avatarlistarray[i].work}/>
+        })
 
-    //use map to loop
-    const avatarcard = avatarlistarray.map((avatarcard, i) => {
-        return <AvatarList id={avatarlistarray[i].id} name={avatarlistarray[i].name} work={avatarlistarray[i].work}/>
-    })
-
-    return (
+        return (
             <div className="mainpage">
                 <h1>Welcome to Avatar World</h1>
                 {avatarcard}
                 <button>Subscribe</button>
             </div>
            )
+
+    }
+    
 }
+
+
+
+    
+
+    
+
 
 export default Avatar;
